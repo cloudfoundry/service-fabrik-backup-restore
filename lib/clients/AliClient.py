2739dbf374450535d51870694d5242bf6e3a26f2
amitmalav <amitmalav96@gmail.com>
2019-03-28 17:18:56 +0530

    Add volume functions

--- lib/clients/AliClient.py
+++ lib/clients/AliClient.py
@@ -3,7 +3,9 @@ from oss2.headers import RequestHeader
 from .BaseClient import BaseClient
 from aliyunsdkcore.request import CommonRequest
 from aliyunsdkcore.client import AcsClient
-
+from ..models.Snapshot import Snapshot
+from ..models.Volume import Volume
+import json
 
 class AliClient(BaseClient):
     def __init__(self, operation_name, configuration, directory_persistent, directory_work_list, poll_delay_time,
@@ -32,6 +34,16 @@ class AliClient(BaseClient):
             msg = 'Could not find or access the given container.'
             self.last_operation(msg, 'failed')
             raise Exception(msg)
+        
+        # skipping some actions for blob operation
+        if operation_name != 'blob_operation':
+            # +-> Get the availability zone of the instance
+            self.availability_zone = self._get_availability_zone_of_server(
+                configuration['instance_id'])
+            if not self.availability_zone:
+                msg = 'Could not retrieve the availability zone of the instance.'
+                self.last_operation(msg, 'failed')
+                raise Exception(msg)
 
     def __setCredentials(self, access_key_id, secret_access_key, region_name):
         self.__aliCredentials = {
@@ -50,7 +62,7 @@ class AliClient(BaseClient):
             raise Exception(
                 'Creation of compute client failed: {}'.format(error))
 
-    def get_common_request(action_name, params, tags=None):
+    def _get_common_request(action_name, params, tags=None):
         request = CommonRequest()
         request.set_domain('ecs.aliyuncs.com')
         request.set_version('2014-05-26')
@@ -69,6 +81,26 @@ class AliClient(BaseClient):
             request.add_query_param(key, params[key])
 
         return request
+
+    def _get_availability_zone_of_server(self, instance_id):
+        try:
+            instance_details_req_params = {
+                'InstanceIds' : [instance_id]
+            }
+            instance_details_request = self._get_common_request('DescribeInstances', instance_details_req_params)
+            instance_details = self.compute_client.do_action_with_exception(instance_details_request)
+            instance_details_json = json.loads(instance_details.decode('utf-8'))
+            if len(instance_details_json['Instances']['Instance']) > 1:
+                message = 'More than 1 instance found for with id {}'.format(
+                instance_id)
+                raise Exception(message)
+            self.logger.error(
+                '[ALI] ERROR: Unable to determine the availability zone of instance{}.\n'.format(instance_id))
+            return None
+        except Exception as error:
+            self.logger.error(
+                '[ALI] ERROR: Unable to determine the availability zone of instance {}.\n{}'.format(instance_id, error))
+            return None
 
     def get_container(self, auth, endpoint):
         try:
@@ -136,7 +168,7 @@ class AliClient(BaseClient):
             }
             self.logger.info('{} START for volume id {} with tags {} and snapshot name {}'.format(
                 log_prefix, volume_id, self.tags, snapshot_name))
-            snpshot_creation_request = get_common_request('CreateSnapshot', snapshot_req_params, self.tags)
+            snpshot_creation_request = self._get_common_request('CreateSnapshot', snapshot_req_params, self.tags)
             snapshot_creation_operation = self.compute_client.do_action_with_exception(snpshot_creation_request)
             snapshot_details_json = json.loads(snapshot_creation_operation.decode('utf-8'))
             snapshot_id = snapshot_details_json['SnapshotId']
@@ -167,7 +199,7 @@ class AliClient(BaseClient):
         return snapshot
 
     
-    def _is_snapshot_ready(snapshot_id, region_id):
+    def _is_snapshot_ready(self, snapshot_id, region_id):
         """Gets the snapshot state.
         https://www.alibabacloud.com/help/doc-detail/25641.htm?spm=a2c63.p38356.a3.5.c880458dkimfOs#SnapshotType
         Status can be "progressing" "accomplished" or "failed"
@@ -180,15 +212,15 @@ class AliClient(BaseClient):
                 'RegionId' : region_id,
                 'SnapshotIds' : [snapshot_id]
             }
-            get_snapshot_request = get_common_request('DescribeSnapshots', get_snapshot_req_params)
+            get_snapshot_request = self._get_common_request('DescribeSnapshots', get_snapshot_req_params)
             snapshot_details = self.compute_client.do_action_with_exception(get_snapshot_request)
             snapshot_details_json = json.loads(snapshot_details.decode('utf-8'))
             if len(snapshot_details_json['Snapshots']['Snapshot']) > 1:
-                self.logger.info('[ALI] ERROR: More than 1 snapshot found for with snapshot id {}'.format(
+                self.logger.error('[ALI] ERROR: More than 1 snapshot found for with snapshot id {}'.format(
                 snapshot_id))
-                return True
+                return False
             snapshot_state = snapshot_details_json['Snapshots']['Snapshot'][0]['Status']
-            if snapshot_state == 'accomplished' || snapshot_state == 'failed':
+            if snapshot_state in ('accomplished','failed'):
                 return True
             return False
         except Exception as error:
@@ -199,30 +231,68 @@ class AliClient(BaseClient):
     def _copy_snapshot(self, snapshot_id):
         return self.get_snapshot(snapshot_id)
 
+    def snapshot_exists(self, snapshot_id):
+        try:
+            region_id = self.__aliCredentials['region_name']
+            get_snapshot_req_params = {
+                'PageSize' : 10,
+                'RegionId' : region_id,
+                'SnapshotIds' : [snapshot_id]
+            }
+            get_snapshot_request = self._get_common_request('DescribeSnapshots', get_snapshot_req_params)
+            snapshot_details = self.compute_client.do_action_with_exception(get_snapshot_request)
+            snapshot_details_json = json.loads(snapshot_details.decode('utf-8'))
+            if len(snapshot_details_json['Snapshots']['Snapshot']) != 0:
+                return False
+            return True
+        except Exception as error:
+            self.logger.error('[ALI] ERROR: Error in getting snapshot {}.\n{}'.format(
+                snapshot_id, error))
+            return None
+
+
+            if len(volume_details_json['Disks']['Disk']) != 0:
+                return False
+            return True
+        except Exception as error:
+            message = '[ALI] ERROR: Unable to get snapshot {}.\n{}'.format(
+                snapshot_id, error)
+            self.logger.error(message)
+            raise Exception(message)
+
     def _delete_snapshot(self, snapshot_id):
         log_prefix = '[SNAPSHOT] [DELETE]'
         try:
             snapshot_deletion_req_params = {
                 'SnapshotId': snapshot_id
             }
-            snpshot_deletion_request = get_common_request('DeleteSnapshot', snapshot_deletion_req_params)
+            snpshot_deletion_request = self._get_common_request('DeleteSnapshot', snapshot_deletion_req_params)
             snapshot_deletion_operation = self.compute_client.do_action_with_exception(snpshot_deletion_request)
 
             self._wait('Waiting for snapshot {} to be deleted...'.format(snapshot_id),
                        lambda id: not self.get_snapshot(id),
                        None,
                        snapshot_id)
-            self._remove_snapshot(snapshot_id)
-            self.logger.info(
-                '{} SUCCESS: snapshot-id={}'.format(
-                    log_prefix, snapshot_id))
-            return True
+            snapshot_exists = self.snapshot_exists(snapshot_id)
+
+            # Check if snapshot exists, if not then it is successfully deleted, else raise exception
+            if not snapshot_exists:
+                self._remove_snapshot(snapshot_id)
+                self.logger.info(
+                    '{} SUCCESS: snapshot-id={}'.format(
+                        log_prefix, snapshot_id))
+                return True
+            else:
+                message = '{} ERROR: snapshot-id={}, snapshot still exists'.format(
+                    log_prefix, snapshot_id)
+                self.logger.error(message)
+                raise Exception(message)
         except Exception as error:
             message = '{} ERROR: snapshot-id={}\n{}'.format(
                 log_prefix, snapshot_id, error)
             self.logger.error(message)
             raise Exception(message)
-    
+
     def get_snapshot(self, snapshot_id):
         try:
             region_id = self.__aliCredentials['region_name']
@@ -231,7 +301,7 @@ class AliClient(BaseClient):
                 'RegionId' : region_id,
                 'SnapshotIds' : [snapshot_id]
             }
-            get_snapshot_request = get_common_request('DescribeSnapshots', get_snapshot_req_params)
+            get_snapshot_request = self._get_common_request('DescribeSnapshots', get_snapshot_req_params)
             snapshot_details = self.compute_client.do_action_with_exception(get_snapshot_request)
             snapshot_details_json = json.loads(snapshot_details.decode('utf-8'))
             if len(snapshot_details_json['Snapshots']['Snapshot']) > 1:
@@ -246,3 +316,208 @@ class AliClient(BaseClient):
             self.logger.error('[ALI] ERROR: Error in getting snapshot {}.\n{}'.format(
                 snapshot_name, error))
             return None
+
+
+    def get_attached_volumes_for_instance(self, instance_id):
+        try:
+            region_id = self.__aliCredentials['region_name']
+            get_attached_volume_req_params = {
+                'PageSize' : 10,
+                'RegionId' : region_id,
+                'InstanceId' : [instance_id]
+            }
+            get_attached_volume_request = self._get_common_request('DescribeDisks', get_attached_volume_req_params)
+            volume_details = self.compute_client.do_action_with_exception(get_attached_volume_request)
+            volume_details_json = json.loads(volume_details.decode('utf-8'))
+
+            volume_list = []
+            for disk in volume_details_json['Disks']['Disk']:
+                device = disk['Device']
+                #  Device information of the related instance, such as /dev/xvdb
+                # It is null unless the Status  is In Use (In_use).
+                if device is not None:
+                    volume_list.append(
+                        Volume(disk['DiskId'], disk['Status'], disk['Size'], device))
+            return volume_list
+        except Exception as error:
+            self.logger.error(
+                '[ALI] ERROR: Unable to find or access attached volume for instance_id {}.{}'.format(
+                    instance_id, error))
+            return []
+
+    def get_persistent_volume_for_instance(self, instance_id):
+        device = self.shell(
+            'cat {} | grep {}'.format(self.FILE_MOUNTS, self.DIRECTORY_PERSISTENT)).split(' ')[0][:8]
+        # --> /dev/vdb on machine will be /dev/xvdb on AliCloud for "I/O Optimized" instances
+        # --> https://www.alibabacloud.com/help/doc-detail/25426.htm
+        device = device.replace('/v', '/xv')
+        for volume in self.get_attached_volumes_for_instance(instance_id):
+            if volume.device == device:
+                self._add_volume_device(volume.id, device)
+                return volume
+        return None
+
+    def _is_volume_ready(self, disk_id, region_id):
+        """Gets the disk state.
+        https://www.alibabacloud.com/help/doc-detail/25626.htm?spm=a2c63.p38356.879954.9.67f065dd0XjNkR#DiskItemType
+        Status can be "In_use" or "Available" or "Attaching" or "Detaching" or "Creating" or "ReIniting"
+        """
+        # Try until disk state is either "Available" or "In_use"
+        # Retries for failures also
+        try:
+            get_disk_req_params = {
+                'PageSize' : 10,
+                'RegionId' : region_id,
+                'DiskIds' : [disk_id]
+            }
+            get_disk_request = self._get_common_request('DescribeDisks', get_disk_req_params)
+            disk_details = self.compute_client.do_action_with_exception(get_disk_request)
+            disk_details_json = json.loads(disk_details.decode('utf-8'))
+            if len(disk_details_json['Disks']['Disk']) > 1:
+                self.logger.error('[ALI] ERROR: More than 1 disks found for with disk_id {}'.format(
+                disk_id))
+                return False
+            disk_state = disk_details_json['Disks']['Disk'][0]['Status']
+            if disk_state in ('Available', 'In_use'):
+                return True
+            return False
+        except Exception as error:
+            self.logger.error(
+                '[ALI] ERROR: Unable to get snapshot details for snapshot id {}.\n{}'.format(snapshot_id, error))
+            return False
+
+    def get_volume(self, volume_id):
+        try:
+            region_id = self.__aliCredentials['region_name']
+            get_volume_req_params = {
+                'PageSize' : 10,
+                'RegionId' : region_id,
+                'DiskIds' : [volume_id]
+            }
+            get_volume_request = self._get_common_request('DescribeDisks', get_volume_req_params)
+            volume_details = self.compute_client.do_action_with_exception(get_volume_request)
+            volume_details_json = json.loads(volume_details.decode('utf-8'))
+            if len(volume_details_json['Disks']['Disk']) > 1:
+                message = 'More than 1 volumes found for with id {}'.format(
+                volume_id)
+                raise Exception(message)
+            if len(volume_details_json['Disks']['Disk']) == 0:
+                return None
+            volume = volume_details_json['Disks']['Disk'][0]
+            return Volume(volume['DiskId'], volume['Status'], volume['Size'])
+        except Exception as error:
+            self.logger.error(
+                '[ALI] ERROR: Unable to find or access volume/disk {}.\n{}'.format(
+                    volume_id, error))
+            return None
+
+    def _create_volume(self, size, snapshot_id=None):
+        log_prefix = '[VOLUME] [CREATE]'
+        volume = None
+        disk_creation_operation = None
+        disk_name = self.generate_name_by_prefix(self.DISK_PREFIX)
+        region_id = self.__aliCredentials['region_name']
+        disk_category = 'cloud_ssd'
+        disk_creation_req_params = {
+            'RegionId' : region_id,
+            'ZoneId' : self.availability_zone,
+            'DiskName' : disk_name,
+            'DiskCategory' : disk_category,
+            'Size' : size
+        }
+        try:
+            if snapshot_id is not None:
+                disk_creation_req_params['SnapshotId'] = snapshot_id
+            
+            disk_creation_request = self._get_common_request('CreateDisk', disk_creation_req_params, self.tags)
+            disk_creation_operation = self.compute_client.do_action_with_exception(disk_creation_request)
+            volume_details_json = json.loads(disk_creation_operation.decode('utf-8'))
+            disk_id = volume_details_json['DiskId']
+
+            self._wait('Waiting for volume {} to get ready...'.format(disk_name),
+                       (lambda disk_id, region_id: self._is_volume_ready(disk_id, region_id)),
+                       None,
+                       disk_id, region_id)
+
+            volume = self.get_volume(disk_id)
+            if volume and volume.status in ('Available', 'In_use'):
+                self._add_volume(volume.id)
+                self.logger.info('{} SUCCESS: volume-id={} with tags={} '.format(
+                    log_prefix, volume.id, self.tags))
+            else:
+                message = '{} ERROR: volume-id={} status={}'.format(
+                    log_prefix, volume.id if volume else None, volume.status if volume else None)
+                raise Exception(message)
+        except Exception as error:
+            message = '{} ERROR: size={}\n{}'.format(log_prefix, size, error)
+            self.logger.error(message)
+            if volume:
+                self.delete_volume(volume.id)
+                volume = None
+            raise Exception(message)
+        return volume
+
+    def volume_exists(self, volume_id):
+        try:
+            region_id = self.__aliCredentials['region_name']
+            get_volume_req_params = {
+                'PageSize' : 10,
+                'RegionId' : region_id,
+                'DiskIds' : [volume_id]
+            }
+            get_volume_request = self._get_common_request('DescribeDisks', get_volume_req_params)
+            volume_details = self.compute_client.do_action_with_exception(get_volume_request)
+            volume_details_json = json.loads(volume_details.decode('utf-8'))
+            if len(volume_details_json['Disks']['Disk']) != 0:
+                return False
+            return True
+        except Exception as error:
+            message = '[ALI] ERROR: Unable to get disk {}.\n{}'.format(
+                volume_id, error)
+            self.logger.error(message)
+            raise Exception(message)
+
+    def _delete_volume(self, volume_id):
+        log_prefix = '[VOLUME] [DELETE]'
+        try:
+            volume_deletion_req_params = {
+                'DiskId': volume_id
+            }
+            volume_deletion_request = self._get_common_request('DeleteDisk', volume_deletion_req_params)
+            volume_deletion_operation = self.compute_client.do_action_with_exception(snpshot_deletion_request)
+
+            self._wait('Waiting for disk {} to be deleted...'.format(volume_id),
+                       lambda id: not self.get_volume(id),
+                       None,
+                       volume_id)
+            
+            self._remove_snapshot(snapshot_id)
+            disk_deletion_operation = self.compute_client.disks().delete(
+                project=self.project_id, zone=self.availability_zone, disk=volume_id).execute()
+
+            self._wait('Waiting for disk {} to be deleted...'.format(volume_id),
+                       (lambda operation_id, zonal_operation: self.get_operation_status(
+                           operation_id, zonal_operation) == 'DONE'),
+                       None,
+                       disk_deletion_operation['name'], True)
+
+            volume_exists = self.volume_exists(volume_id)
+
+            # Check if volume exists, if not then it is successfully deleted, else raise exception
+            if not volume_exists:
+                self._remove_volume(volume_id)
+                self.logger.info(
+                    '{} SUCCESS: volume-id={}'.format(
+                        log_prefix, volume_id))
+                return True
+            else:
+                message = '{} ERROR: volume-id={}, volume still exists'.format(
+                    log_prefix, volume_id)
+                self.logger.error(message)
+                raise Exception(message)
+        except Exception as error:
+            message = '{} ERROR: volume-id={}\n{}'.format(
+                log_prefix, volume_id, error)
+            self.logger.error(message)
+            raise Exception(message)
+
