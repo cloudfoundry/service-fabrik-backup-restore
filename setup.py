6300e1edc46c9c9bb8be7c4c1a80ab7384707094
Anoop Joseph Babu <anoop.joseph.babu@sap.com>
2022-06-26 18:20:02 +0530

    Fix links after GH migration

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class PyTest(TestCommand):
         self.test_args = [ 'tests/', '-vv']
         if self.coverage:
             self.test_args += ['--cov=lib', '--cov-report', 'html']
-        
+
         if self.capture:
             self.test_args += ['--capture=no']
 
@@ -51,17 +51,17 @@ class PyTest(TestCommand):
 settings.update(
     name='service_fabrik_backup_restore',
     version='1',
-    description='Backup & Restore Python Library consumable by Cloud Foundry Services managed by the Service Fabrik',
-    long_description='See README.md on https://github.com/SAP/service-fabrik-backup-restore',
+    description=
+    'Backup & Restore Python Library consumable by Cloud Foundry Services managed by the Service Fabrik',
+    long_description=
+    'See README.md on https://github.com/cloudfoundry/service-fabrik-backup-restore',
     author=[],
     license='Apache 2.0',
     url='http://',
-    keywords="service fabrik service-fabrik backup restore backup-restore broker cloud foundry",
+    keywords=
+    "service fabrik service-fabrik backup restore backup-restore broker cloud foundry",
     install_requires=requirements,
     tests_require=requirements_complete,
-    cmdclass = {
-        'test': PyTest
-    }
-)
+    cmdclass={'test': PyTest})
 
 setup(**settings)
